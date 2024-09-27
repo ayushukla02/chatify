0d4ad7ec72e9419b9bfaee41cd9ca2e294ae863c
Ayush Shukla <ayushukla02@gmail.com>
2024-09-28 05:04:23 +0530

    first

---
+++ server/client/build/precache-manifest.b702daff8a2c0fdac46831d39b6665ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ec7808401528f843bc4ffcca1f97b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81c927fd9b176ecfc78f",
+    "url": "/static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "cbcf5a3d5fac9c349e67",
+    "url": "/static/js/2.56606c3c.chunk.js"
+  },
+  {
+    "revision": "81c927fd9b176ecfc78f",
+    "url": "/static/js/main.939bd625.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
